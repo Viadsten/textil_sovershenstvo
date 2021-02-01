055b88e1f337ec36dda24ee5e2fc4384b63190c5
Viadsten <stanulevich.vg@yandex.ru>
2021-02-01 14:39:29 +0300

    some fix

--- script.js
+++ script.js
@@ -10,4 +10,24 @@ content.addEventListener('scroll', function(e) {
 });
 var event = new Event('scroll');
 
+//картинки с проектами
+var wpCell = document.querySelectorAll('.wlp-cell-js'),
+    wpImg = document.querySelectorAll('.wlp-img-js'),
+    btnImg = document.querySelector('.more-btn--wlp');
 
+var imgEndCount = 16,
+    imgStartCount = 7;
+
+for (let i = 8; i < wpImg.length; i++){
+  wpImg[i].classList.add('wp-cell-js-hide');
+  wpCell[i].classList.add('wp-cell-js-hide');
+}
+
+btnImg.onclick = function(){
+  for (let i = imgStartCount; i < imgEndCount; i++){
+    wpImg[i].classList.remove('wp-cell-js-hide');
+    wpCell[i].classList.remove('wp-cell-js-hide');
+  }
+  imgEndCount = wpImg.length;
+  imgStartCount = 16;
+}
